84f9bfd3266a9289a1ba7bafc28049340917af8c
Lauren Hughes <lh0xfb@gmail.com>
2016-01-08 04:18:40 -0600

    interval beam casting work

---
+++ interval3.h
@@ -0,0 +1,102 @@
+#ifndef INTERVAL3_H
+#define INTERVAL3_H
+
+#include "glm/glm.hpp"
+#include <algorithm>
+
+struct Interval3{
+	glm::vec3 min, max;
+	Interval3(const glm::vec2& x, const glm::vec2& y, const glm::vec2& z)
+		: min(x.x, y.x, z.x), max(x.y, y.y, z.y){};
+	Interval3(const glm::vec3& l, const glm::vec3& u)
+		: min(l), max(u){};
+	Interval3(const Interval3& o) : min(o.min), max(o.max){};
+	inline Interval3& operator + (const Interval3& other){
+		min = min + other.min;
+		max = max + other.max;
+		return *this;
+	}
+	inline Interval3& operator - (const Interval3& other){
+		min = min = other.max;
+		max = max = other.min;
+		return *this;
+	}
+	inline Interval3& operator * (const Interval3& other){
+		float temp[4];
+		temp[0] = min.x*other.min.x;
+		temp[1] = min.x*other.max.x;
+		temp[2] = max.x*other.min.x;
+		temp[3] = max.x*other.max.x;
+		min.x = std::min(temp[0], std::min(temp[1], std::min(temp[2], temp[3])));
+		max.x = std::max(temp[0], std::max(temp[1], std::max(temp[2], temp[3])));
+		temp[0] = min.y*other.min.y;
+		temp[1] = min.y*other.max.y;
+		temp[2] = max.y*other.min.y;
+		temp[3] = max.y*other.max.y;
+		min.y = std::min(temp[0], std::min(temp[1], std::min(temp[2], temp[3])));
+		max.y = std::max(temp[0], std::max(temp[1], std::max(temp[2], temp[3])));
+		temp[0] = min.z*other.min.z;
+		temp[1] = min.z*other.max.z;
+		temp[2] = max.z*other.min.z;
+		temp[3] = max.z*other.max.z;
+		min.z = std::min(temp[0], std::min(temp[1], std::min(temp[2], temp[3])));
+		max.z = std::max(temp[0], std::max(temp[1], std::max(temp[2], temp[3])));
+		return *this;
+	}
+	inline Interval3& operator / (const Interval3& other){
+		Interval3 inv(1.0f / other.max, 1.0f / other.min);
+		*this = *this * inv;
+		return *this;
+	}
+	inline void opUnion(const Interval3& other){
+		min.x = std::min(min.x, other.min.x);
+		min.y = std::min(min.y, other.min.y);
+		min.z = std::min(min.z, other.min.z);
+		max.x = std::max(max.x, other.max.x);
+		max.y = std::max(max.y, other.max.y);
+		max.z = std::max(max.z, other.max.z);
+	}
+	inline void opIntersect(const Interval3& other){
+		min.x = std::max(min.x, other.min.x);
+		min.y = std::max(min.y, other.min.y);
+		min.z = std::max(min.z, other.min.z);
+		max.x = std::min(max.x, other.max.x);
+		max.y = std::min(max.y, other.max.y);
+		max.z = std::min(max.z, other.max.z);
+	}
+	inline void translate(const glm::vec3& v){
+		min += v;
+		max += v;
+	}
+	inline void scale(const glm::vec3& v){
+		min *= v;
+		max *= v;
+	}
+	inline void transform(const glm::mat4& m){
+		glm::vec4 v(min.x, min.y, min.z, 1.0f);
+		v = m * v;
+		min.x = v.x / v.w;
+		min.y = v.y / v.w;
+		min.z = v.z / v.w;
+		v.x = max.x;
+		v.y = max.y;
+		v.z = max.z;
+		v.w = 1.0f;
+		v = m * v;
+		max.x = v.x / v.w;
+		max.y = v.y / v.w;
+		max.z = v.z / v.w;
+	}
+	inline float volume(){
+		glm::vec3 dim = max - min;
+		return dim.x * dim.y * dim.z;
+	}
+};
+
+inline glm::vec2 IASphere(const Interval3& p, const glm::vec3& c, const float r){
+	const float common = glm::dot(c,c) + r*r;
+	const glm::vec2 v(glm::dot(p.min, p.min) - common, glm::dot(p.max, p.max) - common);
+	return glm::vec2(std::min(v.x, v.y), std::max(v.x, v.y));
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -7,115 +7,82 @@
 #include "input.h"
 #include "glprogram.h"
 #include "debugmacro.h"
+#include "interval3.h"
 
 using namespace std;
 using namespace glm;
 
-struct Interval3{
-	glm::vec3 min, max;
-	Interval3(const glm::vec2& u, const glm::vec2& v, const vec2& t)
-		: min(u.x, u.y, t.x), max(v.x, v.y, t.y){};
-	Interval3(const glm::vec3& l, const glm::vec3& u)
-		: min(l), max(u){};
-	Interval3& operator + (const Interval3& other){
-		min = min + other.min;
-		max = max + other.max;
-		return *this;
-	}
-	Interval3& operator - (const Interval3& other){
-		min = min = other.max;
-		max = max = other.min;
-		return *this;
-	}
-	Interval3& operator * (const Interval3& other){
-		float temp[4];
-		temp[0] = min.x*other.min.x;
-		temp[1] = min.x*other.max.x;
-		temp[2] = max.x*other.min.x;
-		temp[3] = max.x*other.max.x;
-		min.x = std::min(temp[0], std::min(temp[1], std::min(temp[2], temp[3])));
-		max.x = std::max(temp[0], std::max(temp[1], std::max(temp[2], temp[3])));
-		temp[0] = min.y*other.min.y;
-		temp[1] = min.y*other.max.y;
-		temp[2] = max.y*other.min.y;
-		temp[3] = max.y*other.max.y;
-		min.y = std::min(temp[0], std::min(temp[1], std::min(temp[2], temp[3])));
-		max.y = std::max(temp[0], std::max(temp[1], std::max(temp[2], temp[3])));
-		temp[0] = min.z*other.min.z;
-		temp[1] = min.z*other.max.z;
-		temp[2] = max.z*other.min.z;
-		temp[3] = max.z*other.max.z;
-		min.z = std::min(temp[0], std::min(temp[1], std::min(temp[2], temp[3])));
-		max.z = std::max(temp[0], std::max(temp[1], std::max(temp[2], temp[3])));
-		return *this;
-	}
-	Interval3& operator / (const Interval3& other){
-		Interval3 inv(1.0f / other.max, 1.0f / other.min);
-		*this = *this * inv;
-		return *this;
-	}
-	Interval3& opUnion(const Interval3& other){
-		min.x = std::min(min.x, other.min.x);
-		min.y = std::min(min.y, other.min.y);
-		min.z = std::min(min.z, other.min.z);
-		max.x = std::max(max.x, other.max.x);
-		max.y = std::max(max.y, other.max.y);
-		max.z = std::max(max.z, other.max.z);
-		return *this;
-	}
-	Interval3& opIntersect(const Interval3& other){
-		min.x = std::max(min.x, other.min.x);
-		min.y = std::max(min.y, other.min.y);
-		min.z = std::max(min.z, other.min.z);
-		max.x = std::min(max.x, other.max.x);
-		max.y = std::min(max.y, other.max.y);
-		max.z = std::min(max.z, other.max.z);
-		return *this;
-	}
-	Interval3& translate(const glm::vec3& v){
-		min += v;
-		max += v;
-		return *this;
-	}
-	Interval3& scale(const glm::vec3& v){
-		min *= v;
-		max *= v;
-		return *this;
-	}
-	Interval3& transform(const glm::mat4& m){
-		glm::vec4 v(min.x, min.y, min.z, 1.0f);
-		v = m * v;
-		min.x = v.x;
-		min.y = v.y;
-		min.z = v.z;
-		v.x = max.x;
-		v.y = max.y;
-		v.z = max.z;
-		v = m * v;
-		max.x = v.x;
-		max.y = v.y;
-		max.z = v.z;
-		return *this;
-	}
-	bool hasZero(){
-		return min.x < 0.0f && max.x > 0.0f && min.y < 0.0f && max.y > 0.0f && min.z < 0.0f && max.z > 0.0f;
-	}
-	float radius(){
-		return 0.5f * glm::distance(min, max);
-	}
-	float volume(){
-		glm::vec3 dim = max - min;
-		return dim.x * dim.y * dim.z;
-	}
-};
+#define WIDTH 800
+#define HEIGHT 600
+#define NEAR -0.1f
+#define FAR -100.0f
+#define MAXDEPTH 64
 
-Interval3 getSphereInterval(vec3& center, float radius){
-	return Interval3(center - radius, center + radius);
+static float depth[HEIGHT][WIDTH];
+
+vec2* BeamCast(const mat4& IVP, const vec2& X, const vec2& Y, const vec2& Z, const float e){
+	Interval3* box = new Interval3(X, Y, Z);	// make an  AABB
+	box->transform(IVP);	// transform to world space
+	vec2 a = IASphere(*box, {0.0f, 0.0f, 0.0f}, 1.0f);	// get base interval
+	delete box;
+	if(a.x > 0.0f || a.y < 0.0f) return nullptr;	// no surface in interval
+	else {
+		if(abs(Z.y - Z.x) < e) return new vec2(Z);	// interval within threshold
+		else {
+			a = vec2(Z.x, (Z.x+Z.y) * 0.5f);
+			vec2* J = BeamCast(IVP, X, Y, a, e);	// test near bisection
+			if(J)
+				return J;
+			else{
+				a = vec2(a.y, Z.y);
+				return BeamCast(IVP, X, Y, a, e); // test far bisection
+			}
+		}
+	}
 }
 
-float beamcast(vec2& u, vec2& v, vec2& t, float e){
-	
-	return 0.0f;
+void Uniform(const mat4& IVP){
+	for(unsigned r = 0; r < HEIGHT; r++){
+		for(unsigned c = 0; c < WIDTH; c++){
+			vec2* di = BeamCast(IVP, vec2(c, c+1), vec2(r, r+1), vec2(-0.1f, -100.0f), 0.001f);
+			if(di)
+				depth[r][c] = (di->x + di->y) * 0.5f;
+		}
+	}
+}
+
+void Paint(const vec2& X, const vec2& Y, vec2* T){
+	if(T){
+		for(int y = Y.x; y < Y.y; y++){
+			for(int x = X.x; x < X.y; x++){
+				depth[y][x] = 0.5f*(T->x + T->y);
+			}
+		}
+	}
+	else{
+		for(int y = Y.x; y < Y.y; y++){
+			for(int x = X.x; x < X.y; x++){
+				depth[y][x] = -100.0f;
+			}
+		}
+	}
+}
+
+void SubDivide(const mat4& IVP, const vec2& X, const vec2& Y, float near, int d, float e){
+	vec2* T = BeamCast(IVP, X, Y, vec2(near, -100.0f), e);
+	if(T == nullptr || d == MAXDEPTH)	// no result or at max depth
+		Paint(X, Y, T);
+	else{
+		vec2 x1(X.x, 0.5f*(X.x+X.y));
+		vec2 x2(x1.y, X.y);
+		vec2 y1(Y.x, 0.5f*(Y.x+Y.y));
+		vec2 y2(y1.x, Y.y);
+		float a = std::min(T->x, T->y);
+		SubDivide(IVP, x1, y1, a, d+1, e*0.5f);
+		SubDivide(IVP, x1, y2, a, d+1, e*0.5f);
+		SubDivide(IVP, x2, y1, a, d+1, e*0.5f);
+		SubDivide(IVP, x2, y2, a, d+1, e*0.5f);
+	}
 }
 
 int main(){
@@ -133,13 +100,7 @@ int main(){
 	
     PointBuffer pb;
     pb.reserve(1024);
-    
-    float zbuffer[1600][900];
-    for(auto& i : zbuffer)
-    	for(auto& j : i)
-    		j = 1024.0f;
-    
-    
+
     
     for(float theta = 0.0f; theta < 3.14f; theta += 0.005f){
     	for(float phi = 0.0f; phi < 6.28f; phi += 0.005f){
