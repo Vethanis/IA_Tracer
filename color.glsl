50a8391d9f78a8e215ebb2a7e889db414426f2cf
Lauren Hughes <lh0xfb@gmail.com>
2016-01-14 21:06:14 -0600

    shading debug targets

--- color.glsl
+++ color.glsl
@@ -33,6 +33,7 @@ void main(){
 #ifndef DISCARD
 	if(z >= 1.) discard;
 #endif
+#ifdef COLOR
 	vec3 pos = getPos(uv.xy, z);
 	vec3 xv  = getPos(uv.xy+ddx, texture(dbuf, suv+.5*ddx).r) - pos;
 	vec3 yv  = getPos(uv.xy+ddy, texture(dbuf, suv+.5*ddy).r) - pos;
@@ -41,23 +42,21 @@ void main(){
 	vec3 H = normalize(L + normalize(eye - pos));
 	float D = max(0.0, dot(N, L));
 	float S = ( D > 0.0f ) ? pow(max(0., dot(H, N)), 32.) : 0.;
-#ifdef COLOR
 	vec3 color = ambient + (D * base_color + S * light_color * base_color) * light_str / dot(light_pos - pos, light_pos - pos);
-	color = pow(color, vec3(1.0f/2.2f));
-#endif
-#ifdef NORMALSHADED
-	vec3 color = ambient + (D * N + S * light_color * N) * light_str / dot(light_pos - pos, light_pos - pos);
-	color = pow(color, vec3(1.0f/2.2f));
-#endif
-#ifdef DEPTH
-	vec3 color = vec3(5.0f - pow(5., texture(dbuf, suv).r));
+	out_color = vec4(pow(color, vec3(1.0f/2.2f)), 1.0);
 #endif
 #ifdef NORMAL
-	vec3 color = N;
+	vec3 pos = getPos(uv.xy, z);
+	vec3 xv  = getPos(uv.xy+ddx, texture(dbuf, suv+.5*ddx).r) - pos;
+	vec3 yv  = getPos(uv.xy+ddy, texture(dbuf, suv+.5*ddy).r) - pos;
+	vec3 N = normalize(cross(xv, yv));
+	out_color = vec4(N, 1.0);
+#endif
+#ifdef DEPTH
+	out_color = vec4(vec3(5.0f - pow(5., texture(dbuf, suv).r)), 1.0);
 #endif
 #ifdef DISCARD
-	vec3 color = vec3(0.0, 1.0, 0.0);
-	if(z >= far) color = vec3(1.0, 0.0, 0.0);
+	out_color = vec4(0.0, 1.0, 0.0, 1.0);
+	if(z >= 1.) out_color = vec4(1.0, 0.0, 0.0, 1.0);
 #endif
-	out_color = vec4(color.rgb, 1.0f);
 }
